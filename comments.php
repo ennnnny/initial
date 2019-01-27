a121513ee398413cf1ab36aa1c10bb71a8b10301
ennnnny <kuye1130@gmail.com>
2019-01-27 17:23:33 +0800

    新增评论表情

--- comments.php
+++ comments.php
@@ -71,6 +71,9 @@ echo $commentClass;
 <input type="email" name="mail" id="mail" class="text" placeholder="邮箱<?php if ($this->options->commentsRequireMail): ?> *<?php endif; ?>" value="<?php $this->remember('mail'); ?>"<?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?> />
 <input type="url" name="url" id="url" class="text" placeholder="http://<?php if ($this->options->commentsRequireURL): ?> *<?php endif; ?>" value="<?php $this->remember('url'); ?>"<?php if ($this->options->commentsRequireURL): ?> required<?php endif; ?> />
 <?php endif; ?>
+<div style="clear:both;top-bottom: 5px;"></div>
+<?php $comments->smilies(); ?>
+<div style="clear:both;padding-bottom: 5px;"></div>
 <button type="submit" class="submit">提交评论</button>
 </p>
 </form>
